28c49510f22305e75240281cbbbbd448d7b4b295
bss <bss>
2021-07-14 20:49:39 +1000

    вёрстка чата

--- Homework/wwwroot/dialogs/dialogs.js
+++ Homework/wwwroot/dialogs/dialogs.js
@@ -1,9 +1,11 @@
 ﻿window.onload = () => {
     document.getElementById('send_button').addEventListener("click", function () {
-        let div = document.createElement('div');
-        let text = document.getElementById('send_text').value;
-        div.className = 'message my_message';
-        div.innerHTML = text;
-        document.getElementsByClassName('message_container')[0].append(div);
+        let div = document.createElement('div')
+        let text = document.getElementById('send_text').value
+        div.className = 'message my_message'
+        div.innerHTML = text
+        var messages = document.getElementsByClassName('message_container')[0]
+        messages.append(div)
+        messages.scrollTop = messages.scrollHeight
     })
 }
